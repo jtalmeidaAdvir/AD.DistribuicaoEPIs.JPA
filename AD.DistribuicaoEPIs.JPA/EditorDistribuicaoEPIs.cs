e5d2f0402c68e3accb0bee4af4f48f3cceee0130
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-11-06 16:17:21 +0000

    first commit

---
+++ AD.DistribuicaoEPIs.JPA/EditorDistribuicaoEPIs.cs
@@ -0,0 +1,18 @@
+﻿using Primavera.Extensibility.BusinessEntities;
+using Primavera.Extensibility.CustomForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AD.DistribuicaoEPIs.JPA
+{
+    public partial class EditorDistribuicaoEPIs : CustomForm
+    {
+        public EditorDistribuicaoEPIs()
+        {
+            InitializeComponent();
+        }
+    }
+}
